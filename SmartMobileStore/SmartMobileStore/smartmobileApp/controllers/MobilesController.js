8d3af56a07afdaebfab7f294726e9dddce598fef
Yember Sai Suman <saisuman30@gmail.com>
2017-07-09 19:28:38 +0530

    Smart Mobile Store App
    Smart Mobile Store App is developed using angularjs 1. It serves as basic template for people who want to develop apps using angularjs1.

---
+++ SmartMobileStore/SmartMobileStore/smartmobileApp/controllers/MobilesController.js
@@ -0,0 +1,27 @@
+﻿(function () {
+	
+	var mobileController = function ($scope) {
+		$scope.mobiles = [
+			{
+				mobileName: "ABC",
+				modelId: 93338,
+				features: ["2GB RAM","32GB Memory","LTE Support"],
+				processor: "Quadcore-Snapdragon-Processor",
+				price:5000
+			},
+			{
+				mobileName: "PQR",
+				modelId:839393,
+				features: ["3GB RAM", "64GB Memory", "LTE Support","Finger Print Sensor"],
+				processor: "OctaCore-Snapdragon-Processor",
+				price: 6000
+			}
+
+
+
+		]
+	};
+
+	app.controller("MobilesController", ["$scope", mobileController]);
+
+}(angular.module("smartmobileModule")));
